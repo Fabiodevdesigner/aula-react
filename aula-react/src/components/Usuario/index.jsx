8ff97f0f02dedf0ad90b47ec6f85e74c9e898106
Fabiodevdesigner <fabiomildesenho@hotmail.com>
2025-05-03 13:36:28 -0300

    alteração aula 2

---
+++ aula-react/src/components/Usuario/index.jsx
@@ -0,0 +1,12 @@
+function Usuario (props)  {
+    return ( 
+        <>
+            <h2>Nome: {props.nome}</h2>
+            <h2>Cidade: Fortaleza</h2>
+            <h2>Idade: 42 Anos</h2>
+        </>
+
+     )
+}
+ 
+export default Usuario
